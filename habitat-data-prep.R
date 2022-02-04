b5700c93861636ca4efd2b953c84fb7bbd6b809b
Lauren E. Hostert <laurenehostert@gmail.com>
2022-02-04 15:01:08 -0600

    Update analysis path and clean habitat data

--- habitat-data-prep.R
+++ habitat-data-prep.R
@@ -5,7 +5,7 @@ library(docstring)
 
 network_prefix <- if_else(as.character(Sys.info()["sysname"]) == "Windows", "//INHS-Bison.ad.uillinois.edu", "/Volumes")
 network_path <- paste0(network_prefix, "/ResearchData/Groups/StreamEcologyLab/CREP")
-analysis_path <- paste0(network_prefix,"/ResearchData/Groups/StreamEcologyLab/CREP/Analysis/Fish/PhaseIII_Analysis")
+analysis_path <- paste0(network_prefix,"/ResearchData/Groups/StreamEcologyLab/CREP/Analysis/Habitat/PhaseIII_Analysis")
 
 
 #### Read Habitat data IN from Access Db Query ####
@@ -24,6 +24,7 @@ dbDisconnect(con)
 hab$Reach_Name <- stringr::str_remove(hab$Reach_Name, "[:blank:]*$")
 hab$PU_Gap_Code  <- stringr::str_remove(hab$PU_Gap_Code, "[:blank:]*$")
 
+
 hab_data <- hab %>%
   filter(lubridate::year(Event_Date) < 2021)
 
@@ -33,20 +34,24 @@ hab_data$Site_ID <-paste(str_replace_all(hab_data$Reach_Name, "[:blank:]", ""), 
 #### Write to File ####
 # write_csv(hab_data, file = paste0(analysis_path, "/Habitat_Metrics_CREP_2013-2020_P3FR.csv"))
 
-id <- read_csv(file = paste0(analysis_path, "/id_table_CREP_2013-2020_P3FR.csv"))
+id <- read_csv(file = paste0(analysis_path, "/Data/id_table_CREP_2013-2020_P3FR.csv"))
 
 
 df_hab <- id %>% 
-  left_join(hab_data)
+  left_join(hab_data) %>%
+  mutate(Visual_Water_Clarity = na_if(Visual_Water_Clarity, ">max depth")) %>% 
+  mutate(Visual_Water_Clarity = na_if(Visual_Water_Clarity, "> max depth")) %>% 
+  mutate(Visual_Water_Clarity = as.numeric(Visual_Water_Clarity))
+
 
 df_hab$Year <- as.factor(lubridate::year(df_hab$Event_Date))
 df_hab$Site_Type <- as.factor(df_hab$Site_Type)
 
 df_hab <- df_hab %>% 
-  select(1:4,117,5,116,6:115)
-
-df_hab <- df_hab %>% 
   mutate(Phase = if_else(Year == 2013|Year == 2014|Year == 2015, 'Phase I', 
                          if_else(Year == 2016|Year == 2017, 'Phase II', 'Phase III'))) %>% 
-  select(1:5,118,6:117)
+  select(1:4,21,22,5:20)
 
+df_hab$Visual_Water_Clarity <- stringr::str_replace(df_hab$Visual_Water_Clarity, ">max depth", "")
+
+write_csv(df_hab, path = paste0(analysis_path, "/Data/Habitat_Metrics_CREP_2013-2020_P3FR.csv"))

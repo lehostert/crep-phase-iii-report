ea53f0fcfd4cd3368f4fe78aa5c1fc96303f60e0
Lauren E. Hostert <laurenehostert@gmail.com>
2022-02-04 14:46:21 -0600

    Update to ingest cleaned habitat data not raw

--- habitat-data-initial-summaries.R
+++ habitat-data-initial-summaries.R
@@ -8,38 +8,21 @@ network_path <- paste0(network_prefix, "/ResearchData/Groups/StreamEcologyLab/CR
 analysis_path <- paste0(network_prefix,"/ResearchData/Groups/StreamEcologyLab/CREP/Analysis/Fish/PhaseIII_Analysis")
 
 
-#### Read Habitat data IN from Access Db Query ####
+#### Read Habitat data IN from habitat-data-prep.R ####
 
-### Connect to Database with odbc ###
-odbcListDrivers() # to get a list of the drivers your computer knows about, be sure there is one for MS Access
-# con <- dbConnect(odbc::odbc(), "Testing_Database")
-con <- dbConnect(odbc::odbc(), "2019_CREP_Database")
-options(odbc.batch_rows = 1) # Must be defined as 1 for Access batch writing or appending to tables.
-dbListTables(con) # To get the list of tables in the database
 
-hab <- as_tibble(tbl(con, "Habitat_Metrics_Matrix"))
-
-dbDisconnect(con)
-
-hab$Reach_Name <- stringr::str_remove(hab$Reach_Name, "[:blank:]*$")
-hab$PU_Gap_Code  <- stringr::str_remove(hab$PU_Gap_Code, "[:blank:]*$")
-
-hab_data <- hab %>%
-  filter(lubridate::year(Event_Date) < 2021)
-
-#### Write Site_ID to match Fish data to Habitat data ####
-hab_data$Site_ID <-paste(str_replace_all(hab_data$Reach_Name, "[:blank:]", ""), str_replace_all(hab_data$Event_Date,"-",""), sep = "_")
 
 #### Write to File ####
-write_csv(site_metric_tibble, file = paste0(analysis_path, "/Habitat_Metrics_CREP_2013-2020_P3FR.csv"))
+df_habitat <- read_csv(site_metric_tibble, file = paste0(analysis_path, "/Habitat_Metrics_CREP_2013-2020_P3FR.csv"))
 
+#### Set defaults and load libraries for visualization ####
+library(ggplot2)
+library(viridis)
 theme_update(plot.title = element_text(hjust = 0.5))
-
-#### Review if this is the informaiton that you need from the habitat analysis. 
 
 
 #### Summary by Site Type per Phase ####
-summary_sites_by_phase_type_1320 <- df %>%
+summary_sites_by_phase_type_1320 <- df_habitat %>%
   pivot_longer(cols = IBI:COSUBPIND, names_to = "Metric", values_to = "Value", values_drop_na = F) %>% 
   group_by(Phase, Site_Type, Metric) %>% 
   summarize(Total_Sites = n_distinct(Site_ID),
@@ -56,7 +39,7 @@ summary_sites_by_phase_type_1320 <- df %>%
 
 
 #### Summary Random Only by Phase ####
-summary_random_sites_by_phase_1320 <- df %>%
+summary_random_sites_by_phase_1320 <- df_habitat %>%
   filter(Site_Type == "random") %>% 
   pivot_longer(cols = IBI:COSUBPIND, names_to = "Metric", values_to = "Value", values_drop_na = F) %>% 
   group_by(Phase, Metric) %>% 
